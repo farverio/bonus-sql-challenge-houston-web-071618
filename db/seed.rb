ec8817f3d1c98dcf3513cce6245cbaf422826866
github username <github email address>
2018-07-28 18:48:22 -0500

    Done.

--- db/seed.rb
+++ db/seed.rb
@@ -1,1 +1,26 @@
-# Parse the CSV and seed the database here! Run 'ruby db/seed' to execute this code.
+require 'sqlite3'
+require 'csv'
+require 'pry'
+
+db = SQLite3::Database.new ":daily_show:"
+
+create_table = <<-SQL 
+  CREATE TABLE IF NOT EXISTS guests (
+    id INTEGER PRIMARY KEY,
+    year INTEGER,
+    occupation TEXT,
+    show_date TEXT,
+    group_type TEXT,
+    guest_name TEXT
+  );
+SQL
+
+db.execute(create_table)
+
+CSV.foreach('./daily_show_guests.csv', headers: true) do |row|
+  insert_row = <<-SQL 
+    INSERT INTO guests (year, occupation, show_date, group_type, guest_name) VALUES (?, ?, ?, ?, ?);
+  SQL
+  
+  db.execute(insert_row, row[0], row[1], row[2], row[3], row[4])
+end
--- lib/queries.rb
+++ lib/queries.rb
@@ -1,5 +1,31 @@
 # Write methods that return SQL queries for each part of the challeng here
 
 def guest_with_most_appearances
-  # write your query here!
+  "SELECT guest_name FROM guests GROUP BY guest_name ORDER BY COUNT(guest_name) DESC LIMIT 1;"
 end
+
+def most_popular_profession_by_year
+  "SELECT year, occupation, COUNT(occupation) FROM guests GROUP BY year;"
+end
+
+def most_popular_profession_overall
+  "SELECT occupation, COUNT(occupation) FROM guests;"
+end
+
+def guests_named_bill
+  "SELECT COUNT(substr(guest_name, 1, pos-1)) AS name_count FROM (SELECT *, INSTR(guest_name, ' ') AS pos FROM guests) GROUP BY first_name HAVING first_name='Bill';"
+end 
+
+def patrick_stewart_dates
+  "SELECT show_date FROM guests WHERE guest_name = 'Patrick Stewart';"
+end 
+
+def highest_guest_year
+  "SELECT year FROM guests GROUP BY year ORDER BY COUNT(year) DESC LIMIT 1;"
+end
+
+def group_popularity_by_year
+  "SELECT year, group_type, COUNT(id) FROM guests GROUP BY year;"
+end
+
+
